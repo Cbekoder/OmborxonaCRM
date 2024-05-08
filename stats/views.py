76b2ee87ac4910706959c1c6aebc31395743afc6
cbekoder <cbekoder@gmail.com>
2024-05-08 16:17:35 +0500

    fixing report link bugs

--- stats/views.py
+++ stats/views.py
@@ -14,68 +14,69 @@ class ReportAPIView(generics.RetrieveAPIView):
         return None
 
     def get(self, request, *args, **kwargs):
+        date_str = request.GET.get('date')
         start_date_str = request.GET.get('start_date')
         end_date_str = request.GET.get('end_date')
 
-        try:
-            start_date = make_aware(datetime.strptime(start_date_str, '%Y-%m-%d'))
-            end_date = make_aware(datetime.strptime(end_date_str, '%Y-%m-%d'))
-        except ValueError:
-            return Response({'error': 'Invalid date format'}, status=status.HTTP_400_BAD_REQUEST)
+        if date_str and (start_date_str or end_date_str):
+            return Response({'error': 'Cannot use both date and start_date/end_date parameters simultaneously'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-        products = Product.objects.all()
+        if date_str:
+            try:
+                date = make_aware(datetime.strptime(date_str, '%Y-%m-%d'))
+            except ValueError:
+                return Response({'error': 'Invalid date format'}, status=status.HTTP_400_BAD_REQUEST)
+
+            products = Product.objects.all()
+            report_data = self.generate_report_data(products, date=date)
+
+        elif start_date_str and end_date_str:
+            try:
+                start_date = make_aware(datetime.strptime(start_date_str, '%Y-%m-%d'))
+                end_date = make_aware(datetime.strptime(end_date_str, '%Y-%m-%d'))
+            except ValueError:
+                return Response({'error': 'Invalid date format'}, status=status.HTTP_400_BAD_REQUEST)
+
+            products = Product.objects.all()
+            report_data = self.generate_report_data(products, start_date=start_date, end_date=end_date)
+
+        else:
+            products = Product.objects.all()
+            report_data = self.generate_report_data(products)
+
+        serializer = self.serializer_class(report_data, many=True)
+        return Response(serializer.data)
+
+    def generate_report_data(self, products, date=None, start_date=None, end_date=None):
         report_data = []
 
         for product in products:
-            goods_in = ProductInput.objects.filter(product=product, created_at__range=(start_date, end_date)).aggregate(
-                goods_in_quantity=Sum('input_quantity'),
-                goods_in_price=Sum('input_quantity') * product.price
-            )
-            first_good_in = ProductInput.objects.filter(product=product, created_at__range=(start_date, end_date)).order_by("created_at").first()
-            last_good_in = ProductInput.objects.filter(product=product, created_at__range=(start_date, end_date)).order_by("created_at").last()
+            goods_in = ProductInput.objects.filter(product=product)
+            goods_out = ProductOutput.objects.filter(product=product)
 
-            goods_out = ProductOutput.objects.filter(product=product, created_at__range=(start_date, end_date)).aggregate(
-                goods_out_quantity=Sum('output_quantity'),
-                goods_out_price=Sum('output_quantity') * product.price
-            )
-            first_good_out = ProductOutput.objects.filter(product=product, created_at__range=(start_date, end_date)).order_by('created_at').first()
-            last_good_out = ProductOutput.objects.filter(product=product, created_at__range=(start_date, end_date)).order_by('created_at').last()
+            if date:
+                goods_in = goods_in.filter(created_at__date=date)
+                goods_out = goods_out.filter(created_at__date=date)
+            elif start_date and end_date:
+                goods_in = goods_in.filter(created_at__range=(start_date, end_date))
+                goods_out = goods_out.filter(created_at__range=(start_date, end_date))
 
-            if first_good_in and first_good_out:
-                if first_good_in.created_at < first_good_out.created_at:
-                    beginning_quantity = first_good_in.all_quantity - first_good_in.input_quantity
-                else:
-                    beginning_quantity = first_good_out.all_quantity + first_good_out.output_quantity
-            elif first_good_in:
-                beginning_quantity = first_good_in.all_quantity - first_good_in.input_quantity
-            elif first_good_out:
-                beginning_quantity = first_good_out.all_quantity + first_good_out.output_quantity
-            else:
-                beginning_quantity = 0  
+            goods_in_quantity = goods_in.aggregate(goods_in_quantity=Sum('input_quantity'))['goods_in_quantity'] or 0
+            goods_in_price = goods_in_quantity * product.price
+
+            goods_out_quantity = goods_out.aggregate(goods_out_quantity=Sum('output_quantity'))['goods_out_quantity'] or 0
+            goods_out_price = goods_out_quantity * product.price
+
+            beginning_quantity = product.quantity or 0
             beginning_price = beginning_quantity * product.price if beginning_quantity else 0
 
-            if last_good_in and last_good_out:
-                if last_good_in.created_at > last_good_out.created_at:
-                    last_quantity = last_good_in.all_quantity
-                else:
-                    last_quantity = last_good_out.all_quantity
-            elif last_good_in:
-                last_quantity = last_good_in.all_quantity
-            elif last_good_out:
-                last_quantity = last_good_out.all_quantity
-            else:
-                last_quantity = 0  
-            last_price = last_quantity * product.price if last_quantity else 0
+            last_quantity = beginning_quantity + goods_in_quantity - goods_out_quantity
+            last_price = beginning_price + goods_in_price - goods_out_price
 
             if all(value == 0 for value in [
-                beginning_quantity,
-                beginning_price,
-                goods_in['goods_in_quantity'] or 0,
-                goods_in['goods_in_price'] or 0,
-                goods_out['goods_out_quantity'] or 0,
-                goods_out['goods_out_price'] or 0,
-                last_quantity,
-                last_price
+                beginning_quantity, beginning_price, goods_in_quantity, goods_in_price,
+                goods_out_quantity, goods_out_price, last_quantity, last_price
             ]):
                 continue
 
@@ -85,15 +86,15 @@ class ReportAPIView(generics.RetrieveAPIView):
                 'product_unit': product.unit.name,
                 'beginning_quantity': beginning_quantity,
                 'beginning_price': beginning_price,
-                'goods_in_quantity': goods_in['goods_in_quantity'] or 0,
-                'goods_in_price': goods_in['goods_in_price'] or 0,
-                'goods_out_quantity': goods_out['goods_out_quantity'] or 0,
-                'goods_out_price': goods_out['goods_out_price'] or 0,
+                'goods_in_quantity': goods_in_quantity,
+                'goods_in_price': goods_in_price,
+                'goods_out_quantity': goods_out_quantity,
+                'goods_out_price': goods_out_price,
                 'last_quantity': last_quantity,
                 'last_price': last_price,
             }
 
             report_data.append(product_report)
 
-        serializer = self.serializer_class(report_data, many=True)
-        return Response(serializer.data)
+        return report_data
+
