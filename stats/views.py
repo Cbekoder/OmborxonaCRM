53e6eb2071eb8470221d3a718c4f1c3ba99c66a2
cbekoder <cbekoder@gmail.com>
2024-05-14 13:55:28 +0500

    stats: adding filter, search and order in report

--- stats/views.py
+++ stats/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from product.models import Product, ProductInput, ProductOutput
+from product.models import Product, ProductInput, ProductOutput, Category
 from .serializers import ReportSerializer
-from django.db.models import Sum
+from django.db.models import Sum, Q
 from datetime import datetime
 from django.utils.timezone import make_aware
 
@@ -17,49 +17,66 @@ class ReportAPIView(generics.RetrieveAPIView):
         date_str = request.GET.get('date')
         start_date_str = request.GET.get('start_date')
         end_date_str = request.GET.get('end_date')
+        category_id = request.GET.get('category')
+        order_by = request.GET.get('order_by')
+        search_query = request.GET.get('search')
 
-        if date_str and (start_date_str or end_date_str):
-            return Response({'error': 'Cannot use both date and start_date/end_date parameters simultaneously'},
-                            status=status.HTTP_400_BAD_REQUEST)
+        products = Product.objects.all()
+
+        # Filtering by category
+        if category_id:
+            products = products.filter(category_id=category_id)
+
+        if search_query:
+            products = products.filter(
+                Q(prod_code__icontains=search_query) |
+                Q(name__icontains=search_query) |
+                Q(category__name__icontains=search_query)
+            )
+
+        # Ordering
+        if order_by:
+            if order_by == 'category':
+                products = products.order_by('category__name')
+            elif order_by == 'name':
+                products = products.order_by('name')
+            elif order_by == 'price':
+                products = products.order_by('price')
+            elif order_by == 'quantity':
+                products = products.order_by('quantity')
+
+        # Fetching report data based on date range
         if date_str:
             try:
                 date = make_aware(datetime.strptime(date_str, '%Y-%m-%d'))
+                products = products.filter(
+                    productinput__created_at__date=date,
+                    productoutput__created_at__date=date
+                )
             except ValueError:
                 return Response({'error': 'Invalid date format'}, status=status.HTTP_400_BAD_REQUEST)
-
-            products = Product.objects.all()
-            report_data = self.generate_report_data(products, date=date)
 
         elif start_date_str and end_date_str:
             try:
                 start_date = make_aware(datetime.strptime(start_date_str, '%Y-%m-%d'))
                 end_date = make_aware(datetime.strptime(end_date_str, '%Y-%m-%d'))
+                products = products.filter(
+                    productinput__created_at__range=(start_date, end_date),
+                    productoutput__created_at__range=(start_date, end_date)
+                )
             except ValueError:
                 return Response({'error': 'Invalid date format'}, status=status.HTTP_400_BAD_REQUEST)
 
-            products = Product.objects.all()
-            report_data = self.generate_report_data(products, start_date=start_date, end_date=end_date)
-
-        else:
-            products = Product.objects.all()
-            report_data = self.generate_report_data(products)
-
+        report_data = self.generate_report_data(products)
         serializer = self.serializer_class(report_data, many=True)
         return Response(serializer.data)
 
-    def generate_report_data(self, products, date=None, start_date=None, end_date=None):
+    def generate_report_data(self, products):
         report_data = []
 
         for product in products:
             goods_in = ProductInput.objects.filter(product=product)
             goods_out = ProductOutput.objects.filter(product=product)
-
-            if date:
-                goods_in = goods_in.filter(created_at__date=date)
-                goods_out = goods_out.filter(created_at__date=date)
-            elif start_date and end_date:
-                goods_in = goods_in.filter(created_at__range=(start_date, end_date))
-                goods_out = goods_out.filter(created_at__range=(start_date, end_date))
 
             goods_in_quantity = goods_in.aggregate(goods_in_quantity=Sum('input_quantity'))['goods_in_quantity'] or 0
             goods_in_price = goods_in_quantity * product.price
@@ -77,7 +94,6 @@ class ReportAPIView(generics.RetrieveAPIView):
                 beginning_quantity, beginning_price, goods_in_quantity, goods_in_price,
                 goods_out_quantity, goods_out_price, last_quantity, last_price
             ]):
-
                 continue
 
             product_report = {
@@ -96,4 +112,3 @@ class ReportAPIView(generics.RetrieveAPIView):
             report_data.append(product_report)
 
         return report_data
-
