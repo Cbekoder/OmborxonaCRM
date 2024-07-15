1b91bb9390862514ebe17a686b2c7eceab024214
cbekoder <cbekoder@gmail.com>
2024-07-15 11:50:09 +0500

    chore: fixing CORD header problem, changing statics input and output (adding product detail

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     # apps
     "users.apps.UsersConfig",
     "stats.apps.StatsConfig",
@@ -49,12 +50,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'config.urls'
 
--- product/serializers.py
+++ product/serializers.py
@@ -34,7 +34,20 @@ class ProductUpdateSerializer(serializers.ModelSerializer):
         fields = ['name', 'description', 'category', 'price', 'unit']
         read_only_fields = ['prod_code']  # prod_code should not be updated
 
-        # INPUT
+
+class ProductDetailInputOutputSerializer(serializers.ModelSerializer):
+    unit = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    class Meta:
+        model = Product
+        fields = ['name', 'prod_code', 'unit', 'category', 'price']
+
+    def get_unit(self, obj):
+        return obj.unit.name
+
+    def get_category(self, obj):
+        return obj.category.name
+# INPUT
 
 
 class ProductInputSerializer(serializers.ModelSerializer):
@@ -42,7 +55,7 @@ class ProductInputSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductInput
-        fields = ['id', 'product', 'input_quantity', 'user_id', "created_at"]
+        fields = ['id', 'product', 'input_quantity', "created_at", 'user_id']
 
     def create(self, validated_data):
         # Retrieve the current user from the request context
@@ -56,18 +69,23 @@ class ProductInputSerializer(serializers.ModelSerializer):
 
 
 class ProductInputGetSerializer(serializers.ModelSerializer):
-    product_name = serializers.SerializerMethodField()
+    product = ProductDetailInputOutputSerializer()
+    user = serializers.SerializerMethodField()
 
     class Meta:
         model = ProductInput
-        fields = ['id', 'product_name', 'all_quantity', 'input_quantity', 'user_id', 'created_at']
+        fields = ['id', 'product', 'input_quantity', 'user', 'created_at']
 
     def get_product_name(self, obj):
         if obj.product:
             return obj.product.name
         return None
 
-        # OUTPUT
+    def get_user(self, obj):
+        return obj.user_id.first_name + ' ' + obj.user_id.last_name
+        # return obj.user_id.username
+
+# OUTPUT
 
 
 class ProductOutputSerializer(serializers.ModelSerializer):
@@ -89,16 +107,15 @@ class ProductOutputSerializer(serializers.ModelSerializer):
 
 
 class ProductOutputGetSerializer(serializers.ModelSerializer):
-    product_name = serializers.SerializerMethodField()
+    product = ProductDetailInputOutputSerializer()
+    user = serializers.SerializerMethodField()
 
     class Meta:
         model = ProductOutput
-        fields = ['id', 'product_name', 'all_quantity', 'output_quantity', 'user_id', 'created_at']
+        fields = ['id', 'product', 'output_quantity', 'user', 'created_at']
 
-    def get_product_name(self, obj):
-        if obj.product:
-            return obj.product.name
-        return None
+    def get_user(self, obj):
+        return obj.user_id.first_name + ' ' + obj.user_id.last_name
 
 class CombinedProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
